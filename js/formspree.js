636ccc0ca7422791d5157a987a81eea582d617db
Kyle Taylor <kyletaylored@gmail.com>
2015-11-12 18:02:16 -0600

    enable ajax

--- js/formspree.js
+++ js/formspree.js
@@ -1,21 +1,21 @@
-// var $contactForm = $('#contact-form');
-// $contactForm.submit(function(e) {
-// 	e.preventDefault();
-// 	$.ajax({
-// 		url: 'https://formspree.io/info@christmas.coffee',
-// 		method: 'POST',
-// 		data: $(this).serialize(),
-// 		dataType: 'json',
-// 		beforeSend: function() {
-// 			$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
-// 		},
-// 		success: function(data) {
-// 			$contactForm.find('.alert--loading').hide();
-// 			$contactForm.append('<div class="alert alert--success">Message sent!</div>');
-// 		},
-// 		error: function(err) {
-// 			$contactForm.find('.alert--loading').hide();
-// 			$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
-// 		}
-// 	});
-// });
+var $contactForm = $('#contact-form');
+$contactForm.submit(function(e) {
+	e.preventDefault();
+	$.ajax({
+		url: 'https://formspree.io/info@christmas.coffee',
+		method: 'POST',
+		data: $(this).serialize(),
+		dataType: 'json',
+		beforeSend: function() {
+			$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+		},
+		success: function(data) {
+			$contactForm.find('.alert--loading').hide();
+			$contactForm.append('<div class="alert alert--success">Message sent!</div>');
+		},
+		error: function(err) {
+			$contactForm.find('.alert--loading').hide();
+			$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+		}
+	});
+});
